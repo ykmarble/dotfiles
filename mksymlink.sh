d7d51afa580bfd9b03ff18be19fb72ea6cdb5527
yk_marble <milkcocoapple@gmail.com>
2013-11-23 15:36:47 +0900

    Change option of find -depth to -maxdepth.

--- mksymlink.sh
+++ mksymlink.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find $(pwd) -depth 1 -name '.[!.]*' -not -name '.git' -exec ln -isv {} $HOME \;
+find $(pwd) -maxdepth 1 -name '.[!.]*' -not -name '.git' -exec ln -isv {} $HOME \;
 echo "finished."
