8778d17ad8e0b9013b15505a7fae8ad901ab7f73
yk_marble <milkcocoapple@gmail.com>
2014-04-11 17:39:50 +0900

    Enabled haskell mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -422,6 +422,17 @@
 (if window-system (require 'caml-font))
 (setq inferior-caml-program "/usr/local/bin/ocaml")
 
+;;; Haskell
+(autoload 'haskell-mode "haskell-mode")
+(autoload 'haskell-cabal "haskell-cabal")
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+(setq haskell-program-name "/usr/bin/ghci")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
 
 ;;; SGML + Zen-coding
 (require 'emmet-mode)
